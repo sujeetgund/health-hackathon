09f8b6b8c37d14e4d55131d558b0aa1f7686054b
Sujeet Gund <sujeetgund@gmail.com>
2025-02-15 12:04:48 +0530

    Fix eslint minor issues

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
